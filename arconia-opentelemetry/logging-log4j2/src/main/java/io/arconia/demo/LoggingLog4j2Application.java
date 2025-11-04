fdcadc56401b454650e3b122fb56e9c18474049a
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-11-04 23:48:41 +0100

    deps: Update Arconia 0.17.1 to 0.18.0
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- arconia-opentelemetry/logging-log4j2/src/main/java/io/arconia/demo/LoggingLog4j2Application.java
+++ arconia-opentelemetry/logging-log4j2/src/main/java/io/arconia/demo/LoggingLog4j2Application.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoggingLog4j2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ObservabilitySignalsApplication.class, args);
+		SpringApplication.run(LoggingLog4j2Application.class, args);
 	}
 
 }

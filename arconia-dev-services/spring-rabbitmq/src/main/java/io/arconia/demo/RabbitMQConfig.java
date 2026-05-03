7cfef9cdcae6bbbf8b7e5c9e1c887f86b3b25185
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2026-05-03 21:29:27 +0200

    Fix RabbitMQ example
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- arconia-dev-services/spring-rabbitmq/src/main/java/io/arconia/demo/RabbitMQConfig.java
+++ arconia-dev-services/spring-rabbitmq/src/main/java/io/arconia/demo/RabbitMQConfig.java
@@ -5,6 +5,7 @@ import tools.jackson.databind.json.JsonMapper;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -24,7 +25,7 @@ class RabbitMQConfig {
 
     @Bean
     Queue queue() {
-        return new Queue(QUEUE_NAME, false);
+        return QueueBuilder.durable(QUEUE_NAME).quorum().build();
     }
 
     @Bean
